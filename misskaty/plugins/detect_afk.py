17c3926777b3cab79795423e062f52f48ec3b3e1
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2023-01-07 11:27:35 +0700

    Update detect_afk.py

--- misskaty/plugins/detect_afk.py
+++ misskaty/plugins/detect_afk.py
@@ -1,14 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiAFKBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiAFKBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-# Modified plugin by me from https://github.com/TeamYukki/YukkiAFKBot to make compatible with pyrogram v2
 import re
 import time
 from utils import put_cleanmode
@@ -17,7 +6,7 @@ from pyrogram import filters, enums
 from database.afk_db import remove_afk, is_afk
 from misskaty.helper.human_read import get_readable_time2
 
-# Detect user that AFK based on Yukki Repo
+# Detect user that AFK based on  Repo
 @app.on_message(
     filters.group & ~filters.bot & ~filters.via_bot,
     group=1,
